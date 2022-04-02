9b6f9c36db60aa685a69813c336c59aa8a48a8f6
Benjamin Lepuschitz <benjaminle@edu.aau.atgit config --global user.name Benjamin>
2022-04-02 17:06:19 +0200

    jacoco plugin + ExampleUnitTest

---
+++ core/src/test/java/com/example/core/ExampleUnitTest.java
@@ -0,0 +1,12 @@
+package com.example.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, MyClassSY.addition(2, 2));
+    }
+}
