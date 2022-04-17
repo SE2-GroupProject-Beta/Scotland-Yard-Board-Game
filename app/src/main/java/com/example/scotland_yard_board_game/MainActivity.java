8a68d07a7f1df7bb5043482cbb29876c4ec6be2e
Klemens Vospernik <klemens.vospernik@gmx.at>
2022-04-17 23:22:24 +0200

    facts_and_figures.md finished, start of client-server-communication

--- app/src/main/java/com/example/scotland_yard_board_game/MainActivity.java
+++ app/src/main/java/com/example/scotland_yard_board_game/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.scotland_yard_board_game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.io.DataOutputStream;
+import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText ipAddress, sendData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ipAddress = (EditText) findViewById(R.id.ipAddress);
+        sendData = (EditText) findViewById(R.id.sendData);
+
     }
+
+    public void button_click(View view) {
+
+    }
+
+    class BackgroundTask extends AsyncTask<String, Void, String> {
+        Socket socket;
+        DataOutputStream dataOutputStream;
+        String ip, message;
+
+
+        @Override
+        protected String doInBackground(String... params) {
+            return null;
+        }
+    }
+
+
 }
