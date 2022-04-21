e7cd0a2e7fc645bcfb0b90dd156b40630e93267a
Benjamin Lepuschitz <benjaminle@edu.aau.atgit config --global user.name Benjamin>
2022-04-21 18:44:17 +0200

    Added JoinGameScreen Added LobbyScreen Added Transitions

--- app/src/main/java/com/example/scotland_yard_board_game/JoinGameScreen.java
+++ app/src/main/java/com/example/scotland_yard_board_game/JoinGameScreen.java
@@ -6,22 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class TitleScreen extends AppCompatActivity {
+public class JoinGameScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_title_screen);
+        setContentView(R.layout.activity_join_game_screen);
     }
 
     public void goLobbyScreen(View view){
 
         Intent LobbyScreen = new Intent(this, LobbyScreen.class);
         startActivity(LobbyScreen);
-    }
-    public void goJoinGameScreen(View view){
-
-        Intent JoinGameScreen = new Intent(this, JoinGameScreen.class);
-        startActivity(JoinGameScreen);
     }
 }
