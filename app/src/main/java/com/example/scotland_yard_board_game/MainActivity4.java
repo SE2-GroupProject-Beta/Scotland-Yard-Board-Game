021961ec5fd5e9486f32301f7861dbcd79956029
Klemens Vospernik <klemens.vospernik@gmx.at>
2022-04-30 18:08:48 +0200

    after restoring MainActivity4 and ClientHandlerThread4

--- app/src/main/java/com/example/scotland_yard_board_game/MainActivity4.java
+++ app/src/main/java/com/example/scotland_yard_board_game/MainActivity4.java
@@ -11,8 +11,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import static com.example.scotland_yard_board_game.ClientHandlerThread3.MESSAGE_TO_PLAYERS;
 
-public class MainActivity3 extends AppCompatActivity {
-    private static final String TAG = "MainActivity3";
+public class MainActivity4 extends AppCompatActivity {
+    private static final String TAG = "MainActivity4";
     Button hostGameButton;
     Button joinGameButton;
     Button sendMessageToServerButton;
@@ -20,16 +20,16 @@ public class MainActivity3 extends AppCompatActivity {
     EditText messageFromServer;
 
     private ServerHandlerThread4 serverHandlerThread =
-            new ServerHandlerThread4("ServerHandlerThread");
-    private ClientHandlerThread3 clientHandlerThread3 =
-            new ClientHandlerThread3("ClientHandlerThread");
+            new ServerHandlerThread4("ServerHandlerThread4");
+    private ClientHandlerThread4 clientHandlerThread =
+            new ClientHandlerThread4("ClientHandlerThread4");
 
     private boolean isMisterX = false; // todo: change to true
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
+        setContentView(R.layout.activity_main4);
 
         hostGameButton = findViewById(R.id.hostGameButton);
         joinGameButton = findViewById(R.id.joinGameButton);
@@ -45,7 +45,7 @@ public class MainActivity3 extends AppCompatActivity {
         if (isMisterX) {
             serverHandlerThread.start();
         }
-        clientHandlerThread3.start();
+        clientHandlerThread.start();
         // Handler clientHandler = new Handler(clientHandlerThread.getLooper());
 
     }
@@ -54,20 +54,20 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         // serverHandlerThread.quit(); // todo: only quit when actually running
-        clientHandlerThread3.quit();
+        clientHandlerThread.quit();
     }
 
     private void sendMessageToServer() {
         Log.d(TAG, "sendMessageToServer");
 
-        Message message = Message.obtain(clientHandlerThread3.getHandler());
+        Message message = Message.obtain(clientHandlerThread.getHandler());
         message.what = MESSAGE_TO_PLAYERS;
         message.obj = "x 114 a 56 b 15 0"; // the actual message as string
 
-        clientHandlerThread3.getHandler().sendMessage(message);
+        clientHandlerThread.getHandler().sendMessage(message);
 
 
-        String transferMessage = clientHandlerThread3.getTransferMessage(); // todo: not correct
+        String transferMessage = clientHandlerThread.getTransferMessage(); // todo: not correct
         messageFromServer.setText(transferMessage);
 
     }
---
+++ app/src/main/java/com/example/scotland_yard_board_game/ServerHandlerThread4.java
@@ -0,0 +1,19 @@
+package com.example.scotland_yard_board_game;
+
+import android.os.HandlerThread;
+
+// built by a YouTube-tutorial by CodingInFlow.com
+// on Threads, Runnables, Handlers, Loopers, MessageQueues and HandlerThreads:
+// my public source code: https://github.com/klemensvo/ThreadsAndHandlers.git
+
+public class ServerHandlerThread4 extends HandlerThread {
+    private static final String TAG = "ServerHandlerThread4";
+
+    public ServerHandlerThread4(String name) { // constructor with identifying name
+        super(name);
+    }
+
+
+
+
+}
