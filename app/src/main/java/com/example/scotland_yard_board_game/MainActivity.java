1599c2d75773269076442735b48c0a35a3c1f898
Klemens Vospernik <klemens.vospernik@gmx.at>
2022-05-04 11:38:06 +0200

    asynchronous handlerthread finished, working and clean environment, for use as merge branch

--- app/src/main/java/com/example/scotland_yard_board_game/MainActivity.java
+++ app/src/main/java/com/example/scotland_yard_board_game/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.scotland_yard_board_game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button goToMainAsync = findViewById(R.id.goToMainAsync);
+        goToMainAsync.setOnClickListener(view -> {
+            Intent MainActivityAsync = new Intent(this, MainActivityAsync.class);
+            startActivity(MainActivityAsync);
+        });
     }
 }
