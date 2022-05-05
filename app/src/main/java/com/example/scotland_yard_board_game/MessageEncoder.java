7a170e4b4600b8cce29f5f496532ee6a516d19df
Klemens Vospernik <klemens.vospernik@gmx.at>
2022-05-06 01:02:03 +0200

    start MessageEncoder class

---
+++ app/src/main/java/com/example/scotland_yard_board_game/MessageEncoder.java
@@ -0,0 +1,32 @@
+package com.example.scotland_yard_board_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MessageEncoder extends AppCompatActivity {
+    /*
+    Encodes and decodes messages between client and server
+
+    client -> server:
+      select station to move to
+
+    server -> client:
+      provide list of all players and after each round
+
+     */
+
+
+    public boolean encodeMessage() {
+        boolean isProperlyEncoded = false;
+
+
+        return isProperlyEncoded;
+    }
+
+    public String playerPositions(int misterX, int playerA, int playerB,
+                                  int PlayerC, int PlayerD, int playerE) {
+        String playerPositions = "";
+
+
+        return playerPositions;
+    }
+}
