8631c68434874336b8bd6fecebe97d8bece56cde
Miirym <56299151+Miirym@users.noreply.github.com>
2022-06-18 19:08:11 +0200

    Squashed commit of the following:
    commit 4ea6dc29053c53a927f077d1740d70331974279c
    Merge: e66837b 6eb7c58
    Author: Benjay2K <85448026+Benjay2K@users.noreply.github.com>
    Date:   Sat Jun 18 13:56:25 2022 +0200
    
        Merge pull request #63 from SE2-GroupProject-Beta/BenjayDev_0621
    
        Benjay dev 0621
    
    commit 6eb7c58d3601b0210efb70f2e3528b1124e96141
    Merge: 5816571 e66837b
    Author: Benjay2K <85448026+Benjay2K@users.noreply.github.com>
    Date:   Sat Jun 18 13:56:15 2022 +0200
    
        Merge branch 'release_0621' into BenjayDev_0621
    
    commit e66837b407c717034ad8c1abf58524188793f627
    Merge: 9a964ab 9f352d8
    Author: Klemens Vospernik <klemens.vospernik@gmx.at>
    Date:   Sat Jun 18 13:42:02 2022 +0200
    
        Merge pull request #62 from SE2-GroupProject-Beta/klemens_develop
    
        player0 on random starting point, works including finding neighbors
    
    commit 5816571ccdbe9b15baac729a1b6e54968ca6e4e1
    Author: Benjamin Lepuschitz <benjaminle@edu.aau.atgit config --global user.name Benjamin>
    Date:   Sat Jun 18 13:41:35 2022 +0200
    
        BlackTickets on JourneyTable
    
    commit 9f352d8697b9eb4fd3bd494de1e7fc9f1574e3ec
    Author: Klemens Vospernik <klemens.vospernik@gmx.at>
    Date:   Sat Jun 18 13:37:58 2022 +0200
    
        player0 on random starting point, works including finding neighbors
    
    commit 9a964ab1d9ee8e357c51c1d179d476b4e8e39a7a
    Merge: 565de3e 2f4236c
    Author: Klemens Vospernik <klemens.vospernik@gmx.at>
    Date:   Sat Jun 18 11:05:18 2022 +0200
    
        Merge pull request #61 from SE2-GroupProject-Beta/klemens_develop
    
        Klemens develop
    
    commit 2f4236cb2e16baacdaa3bb1e7808fc4a741fd334
    Author: Klemens Vospernik <klemens.vospernik@gmx.at>
    Date:   Sat Jun 18 09:56:32 2022 +0200
    
        refactored GameScreen, toast of chosenStation and chosenTransport working
    
    commit 551870d1c33a503f856844273265254973d84218
    Author: Benjamin Lepuschitz <benjaminle@edu.aau.atgit config --global user.name Benjamin>
    Date:   Sat Jun 18 00:13:58 2022 +0200
    
        turnCounter
        PopupMenu for Mr. X abilities in JourneyTable
    
    commit c351b7c269143575a72420efab851d08adb2c619
    Author: Klemens Vospernik <klemens.vospernik@gmx.at>
    Date:   Thu Jun 16 22:30:36 2022 +0200
    
        correct identification of all neighbor stations
    
    commit 565de3e2404c037bdb9a020330e828c9c9cb37ff
    Merge: b9f5205 b716b0c
    Author: Miirym <56299151+Miirym@users.noreply.github.com>
    Date:   Thu Jun 16 21:13:58 2022 +0200
    
        Merge pull request #60 from SE2-GroupProject-Beta/Samer_dev
    
        Samer dev
    
    commit 0141919bb0add611e5f0955344781fea650b036b
    Author: Klemens Vospernik <klemens.vospernik@gmx.at>
    Date:   Thu Jun 16 15:08:04 2022 +0200
    
        refactoring of six players, new initialization
    
    commit 47f98a0f09b82ecc00b8fe6b69b135dc2679b3c4
    Author: Klemens Vospernik <klemens.vospernik@gmx.at>
    Date:   Thu Jun 16 14:22:26 2022 +0200
    
        six players and neighbors moving along with movement of board
    
    commit c2c63b952e509c95cce39cbe5ba41664240be07e
    Author: Klemens Vospernik <klemens.vospernik@gmx.at>
    Date:   Thu Jun 16 12:10:18 2022 +0200
    
        placed 6 players
    
    commit f163a6750ed6170b3d3c2bc55d19a198afea9086
    Author: Klemens Vospernik <klemens.vospernik@gmx.at>
    Date:   Wed Jun 15 11:59:41 2022 +0200
    
        started 6 players

--- app/src/androidTest/java/com/example/scotland_yard_board_game/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/scotland_yard_board_game/ExampleInstrumentedTest.java
@@ -1,9 +1,12 @@
 package com.example.scotland_yard_board_game;
 
 import android.content.Context;
+import androidx.test.runner.AndroidJUnitRunner; // todo: from Fabian
+
 
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +18,7 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
     @Test
